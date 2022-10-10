848c87bd2bea14d73008fe611d4c2006ade0ff32
OmarAlAkkad <omarakkad97@gmail.com>
2022-10-10 18:41:08 -0500

    creating alpha beta and pi matrices

--- kreyol_unsupervised.py
+++ kreyol_unsupervised.py
@@ -16,9 +16,9 @@ def load_dataset(name):
           for line in f:
             word.append(line[0])
             labels.append(line[1])
-        
+
         return word, labels
-    
+
 
 def tag_words(labels):
     res = []
@@ -31,81 +31,189 @@ def tag_words(labels):
           for item in letter[1:]:
             temp.append((item, 'I'))
       res.append(temp)
-      
+
     return res
 
-def create_hmm_dict(tagged_letters):
-    tags = {}
-    count_dict = {}
+def create_beta_dict(tagged_letters, vocab, tags):
+    beta_dict = {}
+    for tag in tags:
+        beta_dict[tag] = {}
+        for key in vocab:
+            beta_dict[tag][key] = 0
     for i in range(len(tagged_letters)):
         for j in range(len(tagged_letters[i])):
-            if tagged_letters[i][j][0] in tags.keys():
-                if tagged_letters[i][j][1] in tags[tagged_letters[i][j][0]].keys():
-                    tags[tagged_letters[i][j][0]][tagged_letters[i][j][1]] += 1
-                else:
-                    tags[tagged_letters[i][j][0]][tagged_letters[i][j][1]] = 1
-            else:
-                tags[tagged_letters[i][j][0]] = {}
-                tags[tagged_letters[i][j][0]][tagged_letters[i][j][1]] = 1
-            if tagged_letters[i][j][1] in count_dict.keys():
-                count_dict[tagged_letters[i][j][1]] += 1
-            else:
-                count_dict[tagged_letters[i][j][1]] = 1
+                    beta_dict[tagged_letters[i][j][1]][tagged_letters[i][j][0]] += 1
 
-    return tags, count_dict
+    return beta_dict
 
-def create_frequency_dict(tags_dict, count_dict):
-    tags_frequency_dict = copy.deepcopy(tags_dict)
-    for key in tags_frequency_dict.keys():
-        for key2 in tags_frequency_dict[key].keys():
-            tags_frequency_dict[key][key2] = tags_frequency_dict[key][key2]/ count_dict[key2]
+def create_beta_frequency_dict(beta_dict):
+    beta_frequency_dict = copy.deepcopy(beta_dict)
+    for key in beta_frequency_dict.keys():
+        count = 0
+        for key2 in beta_frequency_dict[key].keys():
+            count += beta_frequency_dict[key][key2]
+        for key3 in beta_frequency_dict[key].keys():
+            beta_frequency_dict[key][key3] = beta_frequency_dict[key][key3] / count
 
-    return tags_frequency_dict, count_dict
+    return beta_frequency_dict
 
-def create_cobigram_dict(labels):
-    bigram_dict = {}
+def create_alpha_dict(labels, tags):
+    alpha_dict = {}
+    for tag in tags:
+        alpha_dict[tag] = {}
+        for key in tags:
+            alpha_dict[tag][key] = 0
+
     for i in range(len(labels)):
         for j in range(len(labels[i])-1):
-            if labels[i][j][1] in bigram_dict.keys():
-                if labels[i][j+1][1] in bigram_dict[labels[i][j][1]].keys():
-                    bigram_dict[labels[i][j][1]][labels[i][j+1][1]] += 1
-                else:
-                    bigram_dict[labels[i][j][1]][labels[i][j+1][1]] = 1
-            else:
-                bigram_dict[labels[i][j][1]] = {}
-                bigram_dict[labels[i][j][1]][labels[i][j+1][1]] = 1
+            alpha_dict[labels[i][j][1]][labels[i][j+1][1]] += 1
 
-    return bigram_dict
+    return alpha_dict
 
-def create_bigram_frequency_dict(bigram_dict):
+def create_alpha_frequency_dict(bigram_dict):
     bigram_frequency_dict = copy.deepcopy(bigram_dict)
     for key in bigram_frequency_dict.keys():
+        count = 0
         for key2 in bigram_frequency_dict[key].keys():
-            bigram_frequency_dict[key][key2] = bigram_frequency_dict[key][key2]/ count_dict[key]
+            count += bigram_frequency_dict[key][key2]
+        for key3 in bigram_frequency_dict[key].keys():
+            bigram_frequency_dict[key][key3] = bigram_frequency_dict[key][key3] / count
 
     return bigram_frequency_dict
 
+def get_lists(tagged):
+    vocab = list(set([w for sent in tagged for (w,t) in sent]))
+    tags = list(set([t for sent in tagged for (w,t) in sent]))
 
+    return vocab, tags
+
+def get_sets(list1, list2):
+    sourcevocab = set([w for s in vocab for w in s])
+    targetvocab = set([w for s in tags for w in s])
+
+    return sourcevocab, targetvocab
+
+def create_pi_dict(labels,tags):
+    freq_dict = {}
+    words_count = 0
+    for tag in tags:
+        freq_dict[tag] = 0
+
+    for i in range(len(labels)):
+        words_count += 1
+        freq_dict[labels[i][0][1]] += 1
+
+    for key in freq_dict.keys():
+        freq_dict[key] = freq_dict[key]/ words_count
+
+    return freq_dict
+
+def predict_from_scratch(sentence, labels, frequency_dict, bigram_dict, sentence_dict):
+    tags = viterbi(sentence, labels, frequency_dict, bigram_dict, sentence_dict)
+    return tags
+
+def viterbi(words, tags, frequency_dict, bigram_dict, sentence_dict):
+    V = {}
+    B = {}
+    tag_list = sentence_dict.keys()
+    tag_dict = dict.fromkeys(sentence_freq_dict.keys())
+    V[0] = copy.deepcopy(tag_dict)
+    for t in tag_list:
+        try:
+            V[0][t] = sentence_dict[t]*frequency_dict[words[0]].get(t,0)
+        except:
+            V[0][t] = 0
+    for i in range(1,len(words)):
+        V[i] = copy.deepcopy(tag_dict)
+        B[i] = copy.deepcopy(tag_dict)
+        for t in tag_list:
+            pair = argmax(V,tag_list,t,i, bigram_dict)
+            B[i][t] = pair[0]
+            try:
+                V[i][t] = pair[1]*frequency_dict[words[i]].get(t,0)
+            except:
+                V[i][t] = 0
+    final_labels = get_best_tag(words, V, B, tag_list)
+    return final_labels
+
+def argmax(V,tag_list,t,i, bigram_dict):
+    ans=-1
+    best=None
+    for s in tag_list:
+        temp=V[i-1][s]*bigram_dict[t][s]
+        if temp > ans:
+            ans = temp
+            best = s
+    return (best,ans)
+
+def get_best_tag(sent, V,B, tags):
+    best_ending = None
+    best_max = -1
+
+    for tag in tags:
+        if V[len(sent) - 1][tag] > best_max:
+            best_max = V[len(sent) - 1][tag]
+            best_ending = tag
+    seq = [best_ending]
+
+    for i in reversed(range(1, len(sent))):
+        seq.append(B[i][seq[-1]])
+    #print(len(seq), len(sent))
+    return seq[::-1]
+
+
+def evaluate(words, tags, frequency_dict, bigram_dict, sentence_dict):
+    sentence = []
+    labels = []
+    final_tags = []
+    correct = 0
+    num = 0
+    for i in range(len(words)):
+      if words[i]=='<S>':
+        num+=1
+        final_tags.append(predict_from_scratch(sentence, labels, frequency_dict, bigram_dict, sentence_dict))
+        final_tags.append('N')
+        sentence = []
+        labels = []
+      else:
+        sentence.append(words[i])
+        labels.append(tags[i])
+    final_tag = [j for i in final_tags for j in i]
+    print(final_tag)
+    for i in range(len(tags)):
+        if final_tag[i] == tags[i]:
+            correct += 1
+    accuracy = correct / len(tags)
+    return accuracy
 
 if __name__ == "__main__":
     file_name = 'train.tsv'
-    
-    words, labels = load_dataset(file_name)
-    
-    training_labels = labels[:650]
-    
-    tagged_training_letters = tag_words(training_labels)
-    
-    tags_dict, count_dict = create_hmm_dict(tagged_training_letters)
-    
-    tags_frequency_dict, count_dict = create_frequency_dict(tags_dict, count_dict)
-    
-    bigram_dict = create_cobigram_dict(tagged_training_letters)
 
-    bigram_frequency_dict = create_bigram_frequency_dict(bigram_dict)
-    
-    
-    
-    
-    
-    
+    words, labels = load_dataset(file_name)
+
+    training_labels = labels[:650]
+
+    tagged_training_letters = tag_words(training_labels)
+
+    tagged_letters = tag_words(labels)
+
+    vocab, tags = get_lists(tagged_letters)
+
+    vocabset, tagsset = get_sets(vocab, tags)
+
+    alpha_dict = create_alpha_dict(tagged_training_letters,tagsset)
+
+    alpha_frequency_dict = create_alpha_frequency_dict(alpha_dict)
+
+    beta_dict = create_beta_dict(tagged_training_letters, vocab, tags)
+
+    beta_frequency_dict = create_beta_frequency_dict(beta_dict)
+
+    pi_freq_dict = create_pi_dict(tagged_training_letters,tags)
+
+    # training_accuracy = evaluate(x_train, y_train, tags_frequency_dict, bigram_frequency_dict, sentence_freq_dict)
+    # dev_accuracy = evaluate(x_dev, y_dev, tags_frequency_dict, bigram_frequency_dict, sentence_freq_dict)
+
+
+
+
