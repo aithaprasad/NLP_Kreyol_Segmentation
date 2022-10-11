c42ce7c877dc1f9869f630c9a31be0cc7394c543
OmarAlAkkad <omarakkad97@gmail.com>
2022-10-11 13:52:20 -0500

    alpha beta functions

--- test2.py
+++ test2.py
@@ -62,7 +62,7 @@ def create_beta_frequency_dict(beta_dict,vocab,tags):
         for letter in vocab:
             count[tag] += beta_dict[tag][letter]
     print(count)
-                    
+
     beta_frequency_dict = copy.deepcopy(beta_dict)
     for key in beta_frequency_dict.keys():
         for key2 in beta_frequency_dict[key].keys():
@@ -87,12 +87,15 @@ def create_alpha_dict(labels, tags):
 
     return alpha_dict
 
-def create_alpha_frequency_dict(bigram_dict, tags,vocab):
+def create_alpha_frequency_dict(bigram_dict, vocab, tags):
     count = {}
     for tag in tags:
         count[tag] = 0
         for tag2 in tags:
             count[tag] += bigram_dict[tag][tag2]
+
+    count['B'] += bigram_dict['B']['.']
+    count['I'] += bigram_dict['I']['.']
     print(count)
     bigram_frequency_dict = copy.deepcopy(bigram_dict)
     for key in bigram_frequency_dict.keys():
@@ -108,8 +111,8 @@ def get_lists(tagged):
     return vocab, tags
 
 def get_sets(list1, list2):
-    sourcevocab = set([w for s in vocab for w in s])
-    targetvocab = set([w for s in tags for w in s])
+    sourcevocab = set([w for s in list1 for w in s])
+    targetvocab = set([w for s in list2 for w in s])
 
     return sourcevocab, targetvocab
 
@@ -128,135 +131,11 @@ def create_pi_dict(labels,tags):
 
     return freq_dict
 
-def predict_from_scratch(sentence, labels, frequency_dict, bigram_dict, sentence_dict):
-    tags = viterbi(sentence, labels, frequency_dict, bigram_dict, sentence_dict)
-    return tags
-
-def viterbi(words, tags, frequency_dict, bigram_dict, sentence_dict):
-    V = {}
-    B = {}
-    tag_list = sentence_dict.keys()
-    tag_dict = dict.fromkeys(sentence_freq_dict.keys())
-    V[0] = copy.deepcopy(tag_dict)
-    for t in tag_list:
-        try:
-            V[0][t] = sentence_dict[t]*frequency_dict[words[0]].get(t,0)
-        except:
-            V[0][t] = 0
-    for i in range(1,len(words)):
-        V[i] = copy.deepcopy(tag_dict)
-        B[i] = copy.deepcopy(tag_dict)
-        for t in tag_list:
-            pair = argmax(V,tag_list,t,i, bigram_dict)
-            B[i][t] = pair[0]
-            try:
-                V[i][t] = pair[1]*frequency_dict[words[i]].get(t,0)
-            except:
-                V[i][t] = 0
-    final_labels = get_best_tag(words, V, B, tag_list)
-    return final_labels
-
-def argmax(V,tag_list,t,i, bigram_dict):
-    ans=-1
-    best=None
-    for s in tag_list:
-        temp=V[i-1][s]*bigram_dict[t][s]
-        if temp > ans:
-            ans = temp
-            best = s
-    return (best,ans)
-
-def get_best_tag(sent, V,B, tags):
-    best_ending = None
-    best_max = -1
-
-    for tag in tags:
-        if V[len(sent) - 1][tag] > best_max:
-            best_max = V[len(sent) - 1][tag]
-            best_ending = tag
-    seq = [best_ending]
-
-    for i in reversed(range(1, len(sent))):
-        seq.append(B[i][seq[-1]])
-    #print(len(seq), len(sent))
-    return seq[::-1]
-
-
-def evaluate(words, tags, frequency_dict, bigram_dict, sentence_dict):
-    sentence = []
-    labels = []
-    final_tags = []
-    correct = 0
-    num = 0
-    for i in range(len(words)):
-      if words[i]=='<S>':
-        num+=1
-        final_tags.append(predict_from_scratch(sentence, labels, frequency_dict, bigram_dict, sentence_dict))
-        final_tags.append('N')
-        sentence = []
-        labels = []
-      else:
-        sentence.append(words[i])
-        labels.append(tags[i])
-    final_tag = [j for i in final_tags for j in i]
-    print(final_tag)
-    for i in range(len(tags)):
-        if final_tag[i] == tags[i]:
-            correct += 1
-    accuracy = correct / len(tags)
-    return accuracy
-
-def fwd_bkw(observations, states, start_prob, trans_prob, emm_prob, end_st):
-    """Forward–backward algorithm."""
-    # Forward part of the algorithm
-    fwd = []
-    for i, observation_i in enumerate(observations):
-        f_curr = {}
-        for st in states:
-            if i == 0:
-                # base case for the forward part
-                prev_f_sum = start_prob[st]
-            else:
-                prev_f_sum = sum(f_prev[k] * trans_prob[k][st] for k in states)
-
-            f_curr[st] = emm_prob[st][observation_i] * prev_f_sum
-
-        fwd.append(f_curr)
-        f_prev = f_curr
-
-    p_fwd = sum(f_curr[k] * trans_prob[k][end_st] for k in states)
-
-    # Backward part of the algorithm
-    bkw = []
-    for i, observation_i_plus in enumerate(reversed(observations[1:] + (None,))):
-        b_curr = {}
-        for st in states:
-            if i == 0:
-                # base case for backward part
-                b_curr[st] = trans_prob[st][end_st]
-            else:
-                b_curr[st] = sum(trans_prob[st][l] * emm_prob[l][observation_i_plus] * b_prev[l] for l in states)
-
-        bkw.insert(0,b_curr)
-        b_prev = b_curr
-
-    p_bkw = sum(start_prob[l] * emm_prob[l][observations[0]] * b_curr[l] for l in states)
-
-    # Merging the two parts
-    posterior = []
-    for i in range(len(observations)):
-        posterior.append({st: fwd[i][st] * bkw[i][st] / p_fwd for st in states})
-
-    assert p_fwd == p_bkw
-    return fwd, bkw, posterior
-
-def example():
-    return fwd_bkw(vocabtuple,
-                   tagsset,
-                   pi_freq_dict,
-                   alpha_frequency_dict,
-                   beta_frequency_dict,
-                   end_state)
+def alpha(word, em, trans, start):
+    print(word)
+    for letter in word:
+        print(letter)
+    return
 
 if __name__ == "__main__":
     file_name = 'train.tsv'
@@ -272,27 +151,25 @@ if __name__ == "__main__":
     vocab, tags = get_lists(tagged_letters)
 
     vocabset, tagsset = get_sets(vocab, tags)
-    
+
     vocabtuple, tagstuple = tuple(vocab), tuple(tags)
 
     alpha_dict = create_alpha_dict(tagged_training_letters,tagsset)
 
-    alpha_frequency_dict = create_alpha_frequency_dict(alpha_dict,vocab,tags)
+    transition_frequency_dict = create_alpha_frequency_dict(alpha_dict,vocab,tags)
 
     beta_dict = create_beta_dict(tagged_training_letters, vocab, tags)
 
-    beta_frequency_dict = create_beta_frequency_dict(beta_dict,vocab,tags)
+    emmision_frequency_dict = create_beta_frequency_dict(beta_dict,vocab,tags)
 
-    pi_freq_dict = create_pi_dict(tagged_training_letters,tags)
-    
+    start_freq_dict = create_pi_dict(tagged_training_letters,tags)
+
     end_state = '.'
-    
-    mm = example()
-    
-    for line in mm:
-        print(line)
-    
-    
+
+    alpha(words[0], emmision_frequency_dict, transition_frequency_dict, start_freq_dict)
+
+
+
 
     # training_accuracy = evaluate(x_train, y_train, tags_frequency_dict, bigram_frequency_dict, sentence_freq_dict)
     # dev_accuracy = evaluate(x_dev, y_dev, tags_frequency_dict, bigram_frequency_dict, sentence_freq_dict)
